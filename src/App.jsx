8dd63e0d7b9c1a4992b69a057b87d9da624ef66c
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-01-22 03:01:16 +0500

    fixing mobile issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
-      <AppRoutes />
-      <ToastContainer />
+      {/* <AppRoutes />
+      <ToastContainer /> */}
+      <h2 className="text-black text-4xl font-semibold">Hello World</h2>
     </>
   );
 }
