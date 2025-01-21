c09519f05d208e92b28832e6794ce5e092bc99d1
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-01-22 02:11:51 +0500

    fixing mobile issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AuthContextProvider from "./context/authContext.jsx";
-import StripeProvider from "./components/Global/StripeProvider.jsx";
 import AddProductProvider from "./context/addProduct.jsx";
 import CartProductContextProvider from "./context/cartProductContext.jsx";
 import SearchedProductProvider from "./context/searchedProductContext.jsx";
