cccb3486f5d2660ddbf7d5c942922578ac539443
simon-m-dignite <shoaib.muhammad@launchbox.pk>
2025-02-26 18:43:38 +0500

    removed phone number

--- src/components/Global/Footer.jsx
+++ src/components/Global/Footer.jsx
@@ -4,6 +4,7 @@ import { FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
+// eoifne
 
 const Footer = () => {
   return (
