67a86236fc6ebd542cb6f267ee776068d7c00498
mathewgarfield <189109425+mathewgarfield@users.noreply.github.com>
2025-12-12 06:36:41 -0800

    Update:scrolling issues

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = `https://backend.markettoll.com/api/v1`;
-export const BASE_URL = `https://dev.markettoll.com/api/v1`;
-// export const STRIPE_PUBLISHABLE_KEY = "pk_live_51OsZBgRuyqVfnlHKwXGmkKnnY60o2JjCdepf5hLJdEeMcoOK1n3pgrfQRVL7JZ1Rb4bwvgHtb2KbRdFssovm7W0500OHyXWtgn";
-export const STRIPE_PUBLISHABLE_KEY = "pk_test_51OsZBgRuyqVfnlHK0Z5w3pTL7ncHPcC75EwkxqQX9BAlmcXeKappekueIzmpWzWYK9L9HEGH3Y2Py2hC7KyVY0Al00przQczPf"
+export const BASE_URL = `https://backend.markettoll.com/api/v1`;
+// export const BASE_URL = `https://dev.markettoll.com/api/v1`;
+export const STRIPE_PUBLISHABLE_KEY = "pk_live_51OsZBgRuyqVfnlHKwXGmkKnnY60o2JjCdepf5hLJdEeMcoOK1n3pgrfQRVL7JZ1Rb4bwvgHtb2KbRdFssovm7W0500OHyXWtgn";
+// export const STRIPE_PUBLISHABLE_KEY = "pk_test_51OsZBgRuyqVfnlHK0Z5w3pTL7ncHPcC75EwkxqQX9BAlmcXeKappekueIzmpWzWYK9L9HEGH3Y2Py2hC7KyVY0Al00przQczPf"
